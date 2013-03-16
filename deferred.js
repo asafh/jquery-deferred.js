ac4e713a95da335d9ada1156537a7f05f095b426
Asafh <asafh.gh@ous.im>
2013-03-16 23:02:26 +0200

    Adding jQuery's deferred unit tests

--- deferred.js
+++ deferred.js
@@ -1,8 +1,10 @@
-(function(exports) {
+(function(exports, globals) {
 	"use strict";
-	var require = typeof require !== "undefined" ? require : function(p) { return this[p]; };
-	var toolous =  require("toolous");
-	
+	var require = typeof require !== "undefined" ? require : function(p) {
+		return globals[p];
+	};
+	var toolous = require("toolous");
+
 	//=============================Callback lists=============================
 	/**
 	 * Utility object to contain a list of callbacks that can be fired with arguments and a context. <br/>
@@ -10,91 +12,94 @@
 	 * and/or to retain last fired arguments and context to send to newly add listeners (i.e. 'memory')
 	 * @param {Object} options optional callback options
 	 * @param {boolean} options.once  true if to remove listeners after firing. (default=false)
- 	 * @param {boolean} options.memory true if to keep the last value (and context) in memory and fire on new listeners (default=true) 
+	 * @param {boolean} options.memory true if to keep the last value (and context) in memory and fire on new listeners (default=true)
 	 */
 	function CallbackList(options) {
 		this._callbacks = [];
 		this.options = toolous.merge({
-			once: false,
-			memory: true
+			once : false,
+			memory : true
 		}, options);
 	}
+
 	/**
 	 * Binds cb as a listener for this CallbackList. <br/>
-	 * If there is a last value stored in memory (only if constructed with memory=true), cb is immediately called (and if once===true) then removed as a listener. 
+	 * If there is a last value stored in memory (only if constructed with memory=true), cb is immediately called (and if once===true) then removed as a listener.
 	 */
 	CallbackList.prototype.add = function(cb) {
 		this._callbacks.push(cb);
-		if(this.firedArgs) { //memory, firedArgs is always trueish if exists (since it's any array)
-			this.fireWith.apply(this,[this.firedContext].concat(this.firedArgs)); //also removes the listener if once===true
+		if (this.firedArgs) {//memory, firedArgs is always trueish if exists (since it is an array)
+			this.fireWith.apply(this, [this.firedContext].concat(this.firedArgs));
+			//also removes the listener if once===true
 		}
 		return this;
 	};
 	/**
 	 * Fires all listeners with the given context and any arguments given after it
 	 * @param {any} context the context to fire the callbacks with
-	 * @param {any...} args the arguments to send all callback listeners 
+	 * @param {any...} args the arguments to send all callback listeners
 	 */
 	CallbackList.prototype.fireWith = function(context) {
-		var args = toolous.toArray(arguments, 1); //ignore context as a parameter to the callback
-		if(this.memory) { //store value in memory
+		var args = toolous.toArray(arguments, 1);
+		//ignore context as a parameter to the callback
+		if (this.memory) {//store value in memory
 			this.firedArgs = args;
 			this.firedContext = context;
 		}
 		toolous.forEach(this._callbacks, function(cb) {
-			cb.apply(context,args);
+			cb.apply(context, args);
 		});
-		if(this.options.once) { //clean listeners
+		if (this.options.once) {//clean listeners
 			this._callbacks = [];
 		}
 		return this;
-	}
+	};
 	/**
 	 * Fires all listeners with the null context and any arguments given to fire.<br/>
 	 * Equals to fireWith(nul,...)
 	 * @param {any...} args the arguments to send all callback listeners
 	 */
-	CallbackList.prototype.fire = function() { //Fire without context
-		return this.fireWith.apply(this,toolous.toArray(arguments,0,null));
+	CallbackList.prototype.fire = function() {//Fire without context
+		return this.fireWith.apply(this, toolous.toArray(arguments, 0, null));
 	};
 	//===========================END Callback lists===========================
-	
+
 	//========================= Finite State Machine =========================
 	/**
 	 * Finite State Machine (Or a flying spaghetti monster). <br/>
-	 * A finite state machine has a state (string), and fires events when the state changes    
- 	 * @param {Object} options Optional options object of the following format:
- 	 * @param {string} options.state the initial state name. (default="initial")
- 	 * @param {Object} options.stateOptions map between state names to their specific options. of the form {stateName: {once:bool, memory: bool, finalState: bool}}
- 	 * @param {boolean} options.once Default once value for states that don't have a specific once value in options.stateOptions.  true if to remove listeners after firing. (default=true)
- 	 * @param {boolean} options.memory Default memory value for states that don't have a specific memory value in options.stateOptions.  true if to keep the last value (and context) in memory and fire on new listeners (default=true)
- 	 * @param {boolean}: options.finalState: Default finalState value for states that don't have a specific finalState value in options.stateOptions. - true if the FSM cannot change states once in this one. (default=false)
+	 * A finite state machine has a state (string), and fires events when the state changes
+	 * @param {Object} options Optional options object of the following format:
+	 * @param {string} options.state the initial state name. (default="initial")
+	 * @param {Object} options.stateOptions map between state names to their specific options. of the form {stateName: {once:bool, memory: bool, finalState: bool}}
+	 * @param {boolean} options.once Default once value for states that don't have a specific once value in options.stateOptions.  true if to remove listeners after firing. (default=true)
+	 * @param {boolean} options.memory Default memory value for states that don't have a specific memory value in options.stateOptions.  true if to keep the last value (and context) in memory and fire on new listeners (default=true)
+	 * @param {boolean}: options.finalState: Default finalState value for states that don't have a specific finalState value in options.stateOptions. - true if the FSM cannot change states once in this one. (default=false)
 	 */
 	function FSM(options) {
 		options = toolous.merge({
-						state: "initial"
-					}, options);
-		
+			state : "initial"
+		}, options);
+
 		this._state = String(options.state);
 		this._listeners = {};
 		this._stateOpts = toolous.nvl(options.statesOptions, {});
 		this._actualStateOpts = {};
-		
+
 		this._stateOptDefs = {
-			once: !!toolous.nvl(options.once, true),
-			memory: !!toolous.nvl(options.memory, true),
-			finalState: !!toolous.nvl(options.finalState, false)
+			once : !!toolous.nvl(options.once, true),
+			memory : !!toolous.nvl(options.memory, true),
+			finalState : !!toolous.nvl(options.finalState, false)
 		};
 	}
-	
+
 	/**
-	 * Returns the actual state options for <code>state</code>, as a merge between the default state options and the specific state options if any 
- 	 * @param {Object} state
+	 * Returns the actual state options for <code>state</code>, as a merge between the default state options and the specific state options if any
+	 * @param {Object} state
 	 */
 	FSM.prototype._getStateOptions = function(state) {
 		var actual = this._actualStateOpts[state];
-		if(!toolous.isDef(actual)) {
-			this._actualStateOpts[state] = actual = toolous.merge({},this._stateOptDefs, this._stateOpts[state]);
+		if (!toolous.isDef(actual)) {
+			this._actualStateOpts[state] = actual = toolous.merge({}, this._stateOptDefs, this._stateOpts[state]);
 		}
 		return actual;
 	};
@@ -106,181 +111,221 @@
 	FSM.prototype.on = function(state, func) {
 		state = String(state);
 		var cbList = this._listeners[state];
-		if(!cbList) { //create CallbackList
+		if (!cbList) {//create CallbackList
 			var cblOptions = this._getStateOptions(state);
 			this._listeners[state] = cbList = new CallbackList(cblOptions);
 		}
 		cbList.add(func);
 	};
-	
+
 	/**
-	 * If state is defined, attempts to change to it, firing any listeners upon change. <br/>  
-	 * Trying to change from a final state has no effect, doesn't any event listeners and returns false. 
- 	 * @param {string} state the state name to change into. undefined has no affect and just retrieves the state.
- 	 * @return the state after the change (if any change occurred) or false if trying to change from a final state.
+	 * If state is defined, attempts to change to it, firing any listeners upon change. <br/>
+	 * Trying to change from a final state has no effect, doesn't any event listeners and returns false.
+	 * @param {string} state the state name to change into. undefined has no affect and just retrieves the state.
+	 * @return the state after the change (if any change occurred) or false if trying to change from a final state.
 	 */
-	FSM.prototype.state = function(state) {
-		if (isDef(state)) { //change
-			var currentStateOptions = this._getStateOptions(this._state); //check if final
-			if(currentStateOptions.finalState) {
-				return false; //Cannot change.
+	FSM.prototype.state = function(state,context) {
+		if (toolous.isDef(state)) {//change
+			var currentStateOptions = this._getStateOptions(this._state);
+			//check if final
+			if (currentStateOptions.finalState) {
+				return false;
+				//Cannot change.
 			}
 			this._state = state = String(state);
-			var args = toolous.toArray(arguments,1); //skipping state
+			var args = toolous.toArray(arguments, 2, context); //removing state and context
+			//skipping state
 			var cbList = this._listeners[state];
-			if(cbList) {
-				cbList.fire.apply(cbList,args);
+			if (cbList) {
+				cbList.fireWith.apply(cbList, args);
 			}
 		}
 		//get
 		return this._state;
 	};
 	//======================= END Finite State Machine =======================
-	
+
 	//=============================== Deferred ===============================
-	var PROMISE_FUNCTIONS = ["state","then", "done", "fail", "always", "pipe", "progress", "promise"];
+	var PROMISE_FUNCTIONS = ["state", "then", "done", "fail", "always", "pipe", "progress"];
 	var STATES = {
-		resolved: { fire: "resolve", listen: "done", 	 thenIndex:0, memory: true, once: true, query: "isResolved", finalState: true},
-		rejected: { fire: "reject",  listen: "fail", 	 thenIndex:1, memory: true, once: true, query: "isRejected", finalState: true},
-		pending:  { fire: "notify",  listen: "progress", thenIndex:2, memory: true, once: false}
+		resolved : {
+			fire : "resolve",
+			listen : "done",
+			thenIndex : 0,
+			memory : true,
+			once : true,
+			query : "isResolved",
+			finalState : true
+		},
+		rejected : {
+			fire : "reject",
+			listen : "fail",
+			thenIndex : 1,
+			memory : true,
+			once : true,
+			query : "isRejected",
+			finalState : true
+		},
+		pending : {
+			fire : "notify",
+			listen : "progress",
+			thenIndex : 2,
+			memory : true,
+			once : false
+		}
 	};
-	
+
 	/**
-	 * Promise exposes the only promise's set of methods from a deferred object. 
- 	 * @param {Object} deferred the deferred object of which methods to expose
- 	 * @param {any} this either a newly created object (when used with new), or an object to copy all of the promise methods to. 
+	 * Promise exposes the only promise's set of methods from a deferred object.
+	 * @param {Object} deferred the deferred object of which methods to expose
+	 * @param {any} this either a newly created object (when used with new), or an object to copy all of the promise methods to.
 	 */
 	function Promise(deferred) {
 		var promise = this;
-		toolous.forEach(PROMISE_FUNCTIONS,function(funcName) {
+		toolous.forEach(PROMISE_FUNCTIONS, function(funcName) {
 			promise[funcName] = function() {
-				var ret = deferred[funcName].apply(deferred,arguments);
-				return ret === deferred ? promise : ret; //Not returning the deferred object.
+				var ret = deferred[funcName].apply(deferred, arguments);
+				return ret === deferred ? promise : ret;
+				//Not returning the deferred object.
 			};
 		});
+		promise.promise = function() {
+			return this;
+		};
 	}
-	
+
 	function Deferred(init) {
-		this._fsm = new FSM({state:"pending",statesOptions: STATES});
-		
+		if (!(this instanceof Deferred)) {//must be called with new.
+			return new Deferred(init);
+		}
+		var deferred = this;
+		this._fsm = new FSM({
+			state : "pending",
+			statesOptions : STATES
+		});
+
 		var promise = new Promise(this);
 		this.promise = function(obj) {
-			if(!isDef(obj)) {
+			if (!toolous.isDef(obj)) {
 				return promise;
-			}
-			else {
-				toolous.merge(obj, promise); //copy properties, instanceof will not work. 
+			} else {
+				toolous.merge(obj, promise);
+				//copy properties, instanceof will not work.
 				return obj;
 			}
 		};
 		
-		if(isDef(init)) {
-			init.call(this,this);
+		toolous.forEachKey(Deferred.prototype, function(name,func) {
+			this[name] = toolous.bind(func,deferred);
+		});
+		this.pipe = this.then;
+
+		if (toolous.isDef(init)) {
+			init.call(this, this);
 		}
-	};
-	
+	}
+
+
 	toolous.forEachKey(STATES, function(state, stateDefinition) {
-		var fire = stateDefinition.fire,
-			listen = stateDefinition.listen,
-			query = stateDefinition.query;
-		
-		Deferred.prototype[listen] = function(cb) { //Add listeners
-			this._fsm.addListener(state,cb);
+		var fire = stateDefinition.fire, listen = stateDefinition.listen, query = stateDefinition.query;
+
+		Deferred.prototype[listen] = function(cb) {//Add listeners
+			this._fsm.on(state, cb);
 			return this;
 		};
 		Deferred.prototype[fire] = function() {
-			this[fire+"With"].apply(this,toolous.toArray(arguments, 0, this));
+			this[fire + "With"].apply(this, toolous.toArray(arguments, 0, this));
 			return this;
 		};
-		Deferred.prototype[fire+"With"] = function(context) {
-			this._fsm.fireWith.apply(this._fsm, toolous.toArray(arguments, 1, state, context));
+		Deferred.prototype[fire + "With"] = function(context) {
+			this._fsm.state.apply(this._fsm, toolous.toArray(arguments, 1, state, context));
 			return this;
 		};
-		
-		if(query) {
+
+		if (query) {
 			Deferred.prototype[query] = function() {
 				return this._fsm.state() === state;
 			};
 		}
 	});
-	
-	
-	Deferred.prototype.always = function(cb) {
+
+	Deferred.prototype.always = function() {
 		return this.done.apply(this, arguments).fail.apply(this, arguments);
 	};
 	Deferred.prototype.state = function() {
 		return this._fsm.state();
 	};
-	Deferred.prototype.then = function(/*doneFilter, failFilter, progressFilter*/) { //Took some inspiration from jQuery's implementation at https://github.com/jquery/jquery/blob/master/src/deferred.js
-		var args = arguments,
-			retDeferred = new Deferred(),
-			me = this;
+	Deferred.prototype.then = function(/*doneFilter, failFilter, progressFilter*/) {//Took some inspiration from jQuery's implementation at https://github.com/jquery/jquery/blob/master/src/deferred.js
+		var args = arguments, retDeferred = new Deferred(), me = this;
 		toolous.forEachKey(STATES, function(state, stateDefinition) {
 			var i = stateDefinition.thenIndex,
-				fire = stateDefinition.fire,
-				listen = stateDefinition.listen,
-				filter = toolous.isFunction(args[i]) && args[i];
+			// fire = stateDefinition.fire,
+			listen = stateDefinition.listen, filter = toolous.isFunction(args[i]) && args[i];
 			me[listen](function() {
-				var filterResult = filter && filter.apply(this,arguments);
-				if(filterResult && toolous.isFunction(filterResult.promise)) { //a deferred object
-					filterResult.promise()[.done(retDeferred.resolve)]
+				var filterResult = filter && filter.apply(this, arguments);
+				if (filterResult && toolous.isFunction(filterResult.promise)) {//a deferred object
+					var filterPromise = filterResult.promise();
+					toolous.forEachKey(STATES, function(_, chainStateDefinition) {
+						filterPromise[chainStateDefinition.listen](retDeferred[chainStateDefinition.fire]);
+					});
+				} else {//value, passed along
+					retDeferred[stateDefinition.fire + "With"].apply(retDeferred, me.promise(), filter ? [filterResult] : arguments);
+					//if no filter pass arguments as is
 				}
 			});
 		});
-		return ret.promise();
+		return retDeferred.promise();
 	};
-	Deferred.prototype.pipe = Deferred.prototype.then;
-	
-	Deferred.when = function() { //"static" method
+
+	exports.when = function() {
 		var args = toolous.toArray(arguments),
 			ret = new Deferred(),
 			remaining = args.length,
 			results = new Array(remaining),
-			single,
-			checkComplete = function() {
-				if(remaining === 0) {
-					ret.resolve(results);
-				}
-			};
-		
-		if(remaining === 1) {
+			single, checkComplete = function() {
+			if (remaining === 0) {
+				ret.resolve(results);
+			}
+		};
+
+		if (remaining === 1) {
 			single = args[0];
-			if(single !== null) {
-				if(single instanceof Deferred) {
+			if (single !== null) {
+				if ( single instanceof Deferred) {
 					return single.promise();
 				}
-				if(single instanceof Promise) {
+				if ( single instanceof Promise) {
 					return single;
 				}
 			}
 			return ret.resolve(single);
 		}
-		
-		ret.progress(function(i,val) { //mark the ith deferred as returned.
+
+		ret.progress(function(i, val) {//mark the ith deferred as returned.
 			--remaining;
 			results[i] = val;
 			checkComplete();
 		});
 		toolous.forEach(args, function(arg, i) {
-			if(isDef(arg) && arg !== null && (arg instanceof Promise || arg instanceof Deferred)) {
+			if (toolous.isDef(arg) && arg !== null && ( arg instanceof Promise || arg instanceof Deferred)) {
 				arg.done(function(value) {
-					ret.notify(i,value);
+					ret.notify(i, value);
 				});
 				arg.fail(function(args) {
 					ret.reject(args);
 				});
-			}
-			else {
-				ret.notify(i,arg); //immidiate value
+			} else {
+				ret.notify(i, arg);
+				//immidiate value
 			}
 		});
-		
-		checkComplete(); //for remaining === 0
-		
+
+		checkComplete();
+		//zero args or all immediate
+
 		return ret.promise();
-	}; 
-	
-	
+	};
+
 	exports.Deferred = Deferred;
 
-})( typeof exports === 'undefined' ? this['deferred'] = {} : exports); 
+})( typeof exports === 'undefined' ? this.deferred = {} : exports, this);
