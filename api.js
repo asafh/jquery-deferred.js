11d20260036c26727d838d03b13f7227d728f9a6
Asafh <asafh.gh@ous.im>
2013-04-24 00:14:18 +0300

    With content :)

--- api.js
+++ api.js
@@ -1,7 +1,20 @@
 YUI.add("yuidoc-meta", function(Y) {
    Y.YUIDoc = { meta: {
-    "classes": [],
-    "modules": [],
-    "allModules": []
+    "classes": [
+        "CallbackList",
+        "Deferred",
+        "FSM",
+        "Promise",
+        "deferred"
+    ],
+    "modules": [
+        "deferred"
+    ],
+    "allModules": [
+        {
+            "displayName": "deferred",
+            "name": "deferred"
+        }
+    ]
 } };
 });
